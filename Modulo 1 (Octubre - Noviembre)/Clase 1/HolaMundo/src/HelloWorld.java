753acba3a24e6bddc74723f589cad6dae56cc050
LagreenEdits <juandavidlaverde1441@gmail.com>
2025-10-24 11:13:34 -0500

    Clase 1 actualizada

---
+++ Modulo 1 (Octubre - Noviembre)/Clase 1/HolaMundo/src/HelloWorld.java
@@ -0,0 +1,10 @@
+// Nombre del archivo: HelloWorld.java
+// Este es tu primer programa en Java
+
+public class HelloWorld {
+    // Este es el método principal (punto de entrada del programa)
+    public static void main(String[] args) {
+        // La siguiente línea imprime "¡Hola, Mundo desde Java!" en la consola
+        System.out.println("¡Hola, Mundo desde Java!");
+    }
+}
